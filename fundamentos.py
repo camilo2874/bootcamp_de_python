9b1f9127e405f5340d05deef663deebb7187841d
camilo velasquez <camilovelasquez@gmail.com>
2024-07-25 01:52:07 -0500

    se agregaron fundamentos de python

---
+++ fundamentos.py
@@ -0,0 +1,14 @@
+name = "Camilo"
+print(type(name))
+print(name[0])
+print(name[1])
+print(name[2])
+
+last_name ="Velasquez Muñoz"
+
+print( name *5 )
+print(len(name))
+print(name.lower())
+print(name.upper())
+print(name.strip())
+print(name + " " + last_name)
