32d8196f09596d16953e9ddb955e9b800fd773e5
camilo velasquez <camilovelasquez@gmail.com>
2024-07-25 00:55:29 -0500

    Avanzamos variables

---
+++ tipos_de_datos.py
@@ -0,0 +1,17 @@
+# Tipos de datos de texto
+'string'
+"string"
+"""string"""
+'''string'''
+
+# Tipos de datos numéricos
+
+4#tipo de dato entero
+4.1# tipo de dato flotante
+
+# Tipos de datos booleanos
+True# True va con T mayuscula
+False# False va con F mayuscula
+
+
+
---
+++ variables/variables.py
@@ -0,0 +1,14 @@
+nombre = True
+bienvenida = f"hola {nombre} feliz noche"
+
+print(bienvenida)
+print("ola" in bienvenida)
+print ("betty"not in bienvenida)
+print ("hola"not in bienvenida)
+
+#CamelCase
+nombreCompletoDeTuProfe = "Jheyson Galvis"
+
+#SnakeCase es la que se utiliza en python
+
+nombre_completo_de_tu_profe ="Jheyson Galvis"
