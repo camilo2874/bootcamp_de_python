26edbf475b7eade5aea9ad554e6e890a077c2c84
camilo velasquez <camilovelasquez@gmail.com>
2024-07-28 01:50:55 -0500

    abordamos operadores aritmeticos

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,34 @@
+#sumas y restas
+
+suma=2024+2000
+print(suma)
+
+resta=2024-2000
+print(resta)
+
+#multiplicacion y division
+
+multiplicacion=23*7
+print(multiplicacion)
+
+division=8/2
+print(division)
+
+#potencia
+
+potencia=2**3
+print(potencia)
+
+#division entera
+
+division_entera=12//4
+print(division_entera)
+
+#modulo
+
+modulo=12%4
+print(modulo)
+
+tipo_de_dato=type(division)
+
+print(tipo_de_dato)
