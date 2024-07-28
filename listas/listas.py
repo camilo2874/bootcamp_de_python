54314f1c99796aa096526b528f64f4d06453c0c1
camilo velasquez <camilovelasquez@gmail.com>
2024-07-28 02:23:36 -0500

    profundizamos listas

---
+++ listas/listas.py
@@ -0,0 +1,34 @@
+to_do=["me levante a las 8 am",
+    "fui a la U a las 9 pm",
+    "estuve en clases hasta la 1 pm ",
+    "almorce ",
+    "saque a pasear a mi perro",]
+print(to_do)
+
+numbers=[1,2,3,4,"cinco"]
+print(numbers)
+print(type(numbers))
+
+mix=["uno",2,3.14,True,[1,2,3]]
+print(mix)
+
+print(len(mix))
+print("elemento uno:",mix[0])
+print("elemento dos:",mix[1])
+print("elemento tres:",mix[2])
+print("elemento cuatro:",mix[3])
+print("elemento cinco:",mix[4])
+
+print(mix[0:2])
+
+numbers=[1,2,100,90.45,3,4,5]
+print(numbers)
+print("Mayor:",max(numbers))
+print("Menor:",min(numbers))
+
+del numbers [-1]
+print(numbers)
+
+del numbers[:2]
+print(numbers)
+
